806e6ad12ebc6e5f5b78ba1a311e46822eda395d
thabangco <thabangcreates@gmail.com>
2020-09-30 22:07:53 +0200

    first commit

---
+++ handler.js
@@ -0,0 +1,22 @@
+const moment = require('moment-timezone');
+
+module.exports.timeinjozi = async (event) => {
+  let timezone = 'America/Denver';
+
+  if (event.queryStringParameters && event.queryStringParameters.timezone) {
+    // eslint-disable-next-line prefer-destructuring
+    timezone = event.queryStringParameters.timezone;
+
+    if (!moment.timezone.names().includes(timezone)) {
+      return {
+        statusCode: '400',
+        body: `Unknown timezone ${timezone}`
+      };
+    }
+  }
+
+  return {
+    statusCode: '200',
+    body: `The time in ${timezone} is: ${moment.timezone(timezone).format()}`
+  };
+};
---
+++ handler.test.js
@@ -0,0 +1,60 @@
+jest.mock('moment-timezone');
+
+const { timezone } = require('moment-timezone');
+const handler = require('./handler');
+
+const EXPECTED_DATE = '2020-09-01 00:00:00';
+const TIMEZONE = 'America/Monterrey';
+const DEFAULT_TIMEZONE = 'America/Denver';
+
+describe('When call handler.timeinjozi', () => {
+  it('Should return the expected date if the provided timezone exists', async () => {
+    const event = {
+      queryStringParameters: {
+        timezone: TIMEZONE
+      }
+    };
+
+    timezone.names = () => { return [TIMEZONE]; };
+
+    timezone.mockImplementation(() => {
+      return {
+        format: () => { return EXPECTED_DATE; }
+      };
+    });
+
+    const response = await handler.timeinjozi(event);
+
+    expect(response.statusCode).toMatch(/200/);
+    expect(response.body).toMatch(`The time in ${TIMEZONE} is: ${EXPECTED_DATE}`);
+  });
+
+  it('Should return the date for the default timezone if none has been specified', async () => {
+    timezone.names = () => { return [TIMEZONE]; };
+
+    timezone.mockImplementation(() => {
+      return {
+        format: () => { return EXPECTED_DATE; }
+      };
+    });
+
+    const response = await handler.timeinjozi({ queryStringParameters: {} });
+
+    expect(response.statusCode).toMatch(/200/);
+    expect(response.body).toMatch(`The time in ${DEFAULT_TIMEZONE} is: ${EXPECTED_DATE}`);
+  });
+
+  it('Should return an error if the provided timezone does not exists', async () => {
+    const event = {
+      queryStringParameters: {
+        timezone: TIMEZONE
+      }
+    };
+    timezone.names = () => { return []; };
+
+    const response = await handler.timeinjozi(event);
+
+    expect(response.statusCode).toMatch(/400/);
+    expect(response.body).toMatch(`Unknown timezone ${TIMEZONE}`);
+  });
+});
